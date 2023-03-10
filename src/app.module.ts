a3ede4279a5de449979a3a365a82a46bbed0d10a
Edint-Lay <wngur89@edint.io>
2023-03-10 13:19:42 +0900

    add : commit 5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
-    UsersModule, ReportsModule,
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal : true
@@ -19,14 +21,15 @@ import { ReportsModule } from './reports/reports.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [__dirname + '/../**/**.entity{.ts,.js}'],
+      entities: [User, Report],
       synchronize: true,
       autoLoadEntities: true,
       charset : 'utf8mb4'
     }),
+    UsersModule,
+    ReportsModule,
   ],
-  controllers: [],
-  providers: [],
-  exports : []
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
