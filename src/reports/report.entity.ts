45f7875a5db54c8c77ae7da7204eea21412d3de9
Edint-Lay <wngur89@edint.io>
2023-03-13 10:36:56 +0900

    git push

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class Report {
@@ -7,4 +8,28 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  /** 경도 longitude */
+  @Column()
+  lng: number;
+
+  /** 위도 latitude */
+  @Column()
+  lat: number;
+
+  /** 주행거리 */
+  @Column()
+  mileage: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user : User;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { PostReportDto } from './dtos/post.report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+
+    constructor(private reportService : ReportsService) {}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    async postReport(@Body() body : PostReportDto ) {
+        return await this.reportService.postReport(body);
+
+    }
+}
